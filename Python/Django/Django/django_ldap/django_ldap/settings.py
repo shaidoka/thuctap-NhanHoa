6b20b7b0440b4b3c9c8efe14e809ed8fd084e970
shaidoka <shaidokaa@gmail.com>
2024-01-08 10:21:16 +0700

    LDAP

--- Python/Django/Django/django_ldap/django_ldap/settings.py
+++ Python/Django/Django/django_ldap/django_ldap/settings.py
@@ -121,3 +121,48 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import ldap
+from django_auth_ldap.config import LDAPSearch, LDAPGroupQuery, GroupOfNamesType, PosixGroupType
+
+AUTH_LDAP_SERVER_URI = 'ldap://45.117.82.30'
+AUTH_LDAP_BIND_DN = 'cn=adduser_1,dc=nhanhoa,dc=local'
+AUTH_LDAP_BIND_PASSWORD = 'slapd'
+AUTH_LDAP_USER_SEARCH = LDAPSearch('dc=nhanhoa,dc=local',ldap.SCOPE.SUBTREE, '(uid=%(user)s)')
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch('dc=example,dc=com',ldap.SCOPE_SUBTREE, '(objectClass=top)')
+AUTH_LDAP_GROUP_TYPE = PosixGroupType(name_attr="cn")
+AUTH_LDAP_MIRROR_GROUPS = True
+
+# Populate the Django user from the LDAP directory
+AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=groups,dc=nhanhoa,dc=local"
+
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+    "username": "uid",
+    "password": "userPassword",
+}
+
+AUTH_LDAP_PROFILE_ATTR_MAP = {
+    "home_directory": "homeDirectory"
+}
+
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_active": "cn=active,ou=groups,dc=nhanhoa,dc=local",
+    "is_staff": "cn=staff,ou=groups,dc=nhanhoa,dc=local",
+    "is_superuser": "cn=superuser,ou=groups,dc=nhanhoa,dc=local"
+}
+
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+AUTH_LDAP_FIND_GROUP_PERMS = True
+AUTH_LDAP_CACHE_TIMEOUT = 3600
+
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+# Keep ModelBackend around for per-user permissions and may be a local superuser
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
